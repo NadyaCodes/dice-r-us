fe5db4e9e8981732f670363cd5b65f13fcabad05
NadyaCodes <nadyacorscadden@gmail.com>
2022-09-05 14:01:49 -0400

    add loading element

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {useState} from 'react'
 
 function App() {
   const [num, setNum] = useState(0)
+
   return (
     <div>
       <Number setNum={setNum}/>
--- src/Dice.jsx
+++ src/Dice.jsx
@@ -1,16 +1,28 @@
 import { makeDice } from './diceHelpers'
+import {useEffect, useState} from 'react'
 
 
 export default function Dice(props) {
   let {num} = props
+  const [showDice, setShowDice] = useState(false)
   const diceArray = []
-
 
   for (let i = 0; i < num; i++) {
     diceArray.push(makeDice())
   }
 
+  useEffect(() => {
+    if (num > 0) {
+      setShowDice(false);
+      setTimeout(() => {
+        setShowDice(true)
+      }, 1000);
+    }
+  }, [num])
+
   return(
-    <div className='diceDisplay'>{diceArray}</div> 
+    <div>
+    {showDice === true ? <div className='diceDisplay'>{diceArray}</div> : <div>Loading...</div>  }
+    </div>
   )
 }
